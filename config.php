30ed022a14380101afa01d9e1f43758ae93e3d4b
subashpaudel <43787823+subashpaudel@users.noreply.github.com>
2019-08-21 23:29:31 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?Php
+$host_name = "localhost";
+$database = "sql_tutorial"; // Change your database nae
+$username = "root";          // Your database user id 
+$password = "test";          // Your password
+
+//////// Do not Edit below /////////
+try {
+$dbo = new PDO('mysql:host='.$host_name.';dbname='.$database, $username, $password);
+} catch (PDOException $e) {
+print "Error!: " . $e->getMessage() . "<br/>";
+die();
+}
+?>
